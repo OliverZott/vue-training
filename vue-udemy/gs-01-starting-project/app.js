0425f9c76ad9a015a5c01a53843ff1eac810decb
Oliver Zott <oliver.zott@agindo.at>
2025-07-23 17:45:54 +0200

    feat: simple vue example via cdn

--- vue-udemy/gs-01-starting-project/app.js
+++ vue-udemy/gs-01-starting-project/app.js
@@ -1,14 +1,36 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+/* Vue: Declerative approach... declare result instead of steps gettting there */
 
-buttonEl.addEventListener('click', addGoal)  // only point top function, its executed on click
+Vue.createApp({
+    // we passing an object to createApp (indicated by the  {})
+    data() {
+        // property named data that has a function as a value. Same as data: function() {}
+        return {
+            goals: [],
+            enteredValue: '',
+        };
+    },
+    methods: {
+        // in the value-objectb we can define funcritons, available in htmSl
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        },
+    },
+}).mount('#app');
 
-function addGoal() {
-    const enteredValue = inputEl.value;
-    const listItemEl = document.createElement('li');
-    listItemEl.textContent = enteredValue;
+/* Vanilla js: imperative aproach - step by step appreach  */
 
-    listEl.appendChild(listItemEl);
-    inputEl.value = '';
-}
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// buttonEl.addEventListener('click', addGoal)  // only point top function, its executed on click
+
+// function addGoal() {
+//     const enteredValue = inputEl.value;
+//     const listItemEl = document.createElement('li');
+//     listItemEl.textContent = enteredValue;
+
+//     listEl.appendChild(listItemEl);
+//     inputEl.value = '';
+// }
