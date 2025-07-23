3c984830945185ba8472dc03dfc872f713a2f5dc
Oliver Zott <oliver.zott@agindo.at>
2025-07-23 16:51:38 +0200

    feat: vanilla js example

--- vue-udemy/gs-01-starting-project/app.js
+++ vue-udemy/gs-01-starting-project/app.js
@@ -2,9 +2,13 @@ const buttonEl = document.querySelector('button');
 const inputEl = document.querySelector('input');
 const listEl = document.querySelector('ul');
 
-buttonEl.addEventListener('click', addGoal)
+buttonEl.addEventListener('click', addGoal)  // only point top function, its executed on click
 
 function addGoal() {
     const enteredValue = inputEl.value;
-    const listItemEl = document.createElement();
+    const listItemEl = document.createElement('li');
+    listItemEl.textContent = enteredValue;
+
+    listEl.appendChild(listItemEl);
+    inputEl.value = '';
 }
