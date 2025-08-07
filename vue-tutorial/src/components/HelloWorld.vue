88e2fd3f399180d7e5600093d53ebef14e6e0a3a
Oliver Zott <oliver.zott@agindo.at>
2025-08-07 16:55:19 +0200

    vue tutorial

--- vue-tutorial/src/components/HelloWorld.vue
+++ vue-tutorial/src/components/HelloWorld.vue
@@ -1,46 +1,52 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from 'vue';
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
-function incrementCount() {
-  count.value++
+const count = ref(0);
+const myMsg = ref('My Text :)');
+
+// Attribute binding via v-bind:class or shorthand :class
+const titleClass = ref('title');
+
+const isTrue = ref(0);
+
+// Event listener for the button click
+function incrementCount(val: number) {
+    count.value = count.value + val;
+}
+
+const inputText = ref('');
+
+function showInputText(e: Event) {
+    inputText.value = e.target.value;
 }
 
 
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+    <h1>{{ msg }}</h1>
+    <h2 :class="titleClass"> {{ myMsg }} </h2>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
+    <!--    Event handler-->
+    <div class="card">
+        <button type="button" @click="incrementCount(3)">count is {{ count }}</button>
+    </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    <!-- two-way binding -->
+    <input type="text" v-model="myMsg" />
+    <input type="text" :value="inputText" @input="showInputText" placeholder="Type here" />
+    <p>{{ inputText }}</p>
+
+    <!--    Conditional rendering-->
+    <p v-if="isTrue">True</p>
+    <p v-else>False</p>
+
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+.title {
+    color: #ee1313;
 }
 </style>
